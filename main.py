487373b12d603a2c763409a72018a7b1ec39ef42
Abdul Wahab <abdul.wahab@gcslahore.edu.pk>
2022-09-25 13:34:00 +0500

    Code  shortened   plus   placement of break  is also adjusted

--- main.py
+++ main.py
@@ -1,20 +1,22 @@
+# Video  URL
+
 def isEven(x):
     return x % 2 == 0
 
-
-v = 1
 no_of_evens = 0
 no_of_odds = 0
-
+v = 1
 while v != -1:
-    v = int(input('Enter Value'))
-    if v != -1:
-        if isEven(v):
-            no_of_evens += 1
-        else:
-            no_of_odds += 1
-    else:
+    v = int(input('Enter a value = '))
+    if v == -1:
         break
 
-print(f'No of Evens are { no_of_evens }')
-print(f'No of Odds are { no_of_odds }')
+    if isEven(v):
+        no_of_evens += 1
+    else:
+        no_of_odds += 1
+
+
+
+print(f'  No of Evens are = {no_of_evens}')
+print(f'  No of Odds are = {no_of_odds}')
